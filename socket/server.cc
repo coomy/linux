6692bfac55f706ab2692e23130074f4a732a0363
coomy <misol.gao@gmail.com>
2017-11-29 20:28:09 +0800

    [TMP] tmp commit for epoll.

--- socket/server.cc
+++ socket/server.cc
@@ -1,8 +1,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/epoll.h> 
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 
 #include <iostream>
@@ -13,6 +15,24 @@
 
 using std::cout;
 using std::endl;
+
+
+/** Returns true on success, or false if there was an error */
+bool SetSocketBlockingEnabled(int fd, bool blocking)
+{
+   if (fd < 0) return false;
+
+#ifdef _WIN32
+   unsigned long mode = blocking ? 0 : 1;
+   return (ioctlsocket(fd, FIONBIO, &mode) == 0) ? true : false;
+#else
+   int flags = fcntl(fd, F_GETFL, 0);
+   if (flags < 0) return false;
+   flags = blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK);
+   return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+#endif
+}
+
 
 int main()
 {
@@ -56,7 +76,58 @@ int main()
     exit(1);
   }
 
+  if (!SetSocketBlockingEnabled(server_fd, true)) {
+    perror("make socket non blocking error.");
+    exit(1);
+  }
+
+  static const int MAX_EVENTS = 10;
+  struct epoll_event event, events[MAX_EVENTS];
+  int event_fd;
+  if (epoll_create1(0)<0) {
+    perror("epoll create error");
+    exit(1);
+  }
+
+  event.data.fd = server_fd;
+  event.events = EPOLLIN | EPOLLET;
+  if (epoll_ctl(event_fd, EPOLL_CTL_ADD, server_fd, &event)<0) {
+    perror("epoll ctl error.");
+    exit(1);
+  }
+
+  events = calloc(MAX_EVENTS, sizeof(event));
+
+
   while (true) {
+    int size_event = epoll_wait(event_fd, events, MAX_EVENTS, -1);
+    for (int i=0; i<size_event; i++) {
+      if (events[i].evnets & (EPOLLERR|EPOLLHUP)) {
+        cout << "epoll error" << endl;
+        close(events[i].data.fd);
+        continue;
+      }
+
+      if (events[i].events & EPOLLIN) {
+        if (server_fd == events[i].data.fd) {
+          conn_sock = accept(sockfd,
+                  (struct sockaddr *) &remote_addr, &sin_size);
+          if (conn_sock == -1) {
+              perror("accept");
+              exit(EXIT_FAILURE);
+          }
+          setnonblocking(conn_sock);
+          ev.events = EPOLLIN | EPOLLET;
+          ev.data.fd = conn_sock;
+          if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock,
+                      &ev) == -1) {
+              perror("epoll_ctl: conn_sock");
+              exit(EXIT_FAILURE);
+          }
+        }
+      }
+    }
+
     cout << "wait connect..." << endl;
     sin_size = sizeof(struct sockaddr_in);
     if ((client_fd = accept(server_fd, 
@@ -88,3 +159,7 @@ int main()
 
 //http://lengly.top/archives/155
 //http://www.cnblogs.com/ggjucheng/archive/2012/01/17/2324584.html
+
+//http://blog.lucode.net/linux/epoll-tutorial.html
+//https://www.felix021.com/blog/read.php?1879
+//http://www.cnblogs.com/zhanggaofeng/p/5895166.html
